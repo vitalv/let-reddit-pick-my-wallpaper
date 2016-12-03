567bc12e7ba65f35d98f73a86726c974ab096d6d
Vital Vialas <uveuve@gmail.com>
2016-12-03 16:57:05 +0100

    cleaned layout

--- r_wallpapers_sets_my_wallpaper.py
+++ r_wallpapers_sets_my_wallpaper.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import praw
 import urllib
 import os
@@ -13,7 +16,7 @@ my_user = 'reddit_user_name'
 my_password = 'not.my.password'
 user_agent='pc:r_wallpapers_sets_my_wallpaper:0.1 '
 
-1 either provide them here, or in a praw.ini file residing in the same directory
+1 either provide them here, or in a praw.ini file residing in the same directory (or at ~/.config/)
 
 reddit = praw.Reddit(client_id=client_id,
 					client_secret=client_secret,
@@ -33,25 +36,47 @@ user_agent=pc:r_wallpapers_sets_my_wallpaper:0.1
 
 '''
 
-reddit = praw.Reddit('user_data')
+def get_top_submission(subreddit):
 
-r_wallpapers = reddit.subreddit('wallpapers')
+	for submission in subreddit.top(time_filter='day', limit=1):
+		preview = submission.preview
+		title = submission.title.replace(" ", "_").strip(".")
+	url = preview["images"][0]["source"]["url"]
+	return url, title
 
-for submission in r_wallpapers.top(time_filter='day', limit=1):
-	preview = submission.preview
-	title = submission.title.replace(" ", "_")
 
-url = preview["images"][0]["source"]["url"]
+def configure_wallpaper_path(title):
+	
+	home_dir = os.path.expanduser('~')
+	wallpapers_dir = os.path.join(home_dir, ".wallpapers")
+	if not os.path.isdir(wallpapers_dir):
+		os.mkdir(wallpapers_dir)
+	wallpaper_path = os.path.join(wallpapers_dir, str(title)+".jpg")
+	return wallpaper_path
 
-home_dir = os.path.expanduser('~')
-wallpapers_dir = os.path.join(home_dir, ".wallpapers")
-if not os.path.isdir(wallpapers_dir):
-	os.mkdir(wallpapers_dir)
-wallpaper_path = os.path.join(wallpapers_dir, str(title)+".jpg")
-urllib.urlretrieve(str(url), wallpaper_path )
-print "Downloaded \"" + title + "\" from " + url + " \nand saved it to " + wallpapers_dir
 
-change_wallpaper_cmd = "gsettings set org.gnome.desktop.background picture-uri file://" + wallpaper_path
+def download_wallpaper(url, path):
 
-subprocess.call(change_wallpaper_cmd, shell=True)
+	if urllib.urlopen(str(url)).getcode() == "200":
+		urllib.urlretrieve(str(url), path )
+		print "Downloaded \"" + title + "\" from " + url + " \nand saved it to " + wallpapers_dir
 
+
+def change_local_wallpaper(wallpaper_path):
+	
+	change_wallpaper_cmd = "gsettings set org.gnome.desktop.background picture-uri file://" + wallpaper_path
+	subprocess.call(change_wallpaper_cmd, shell=True)
+
+
+def main():
+
+	reddit = praw.Reddit('user_data')
+	r_wallpapers = reddit.subreddit('wallpapers')
+	url, title = get_top_submission(r_wallpapers)
+	wallpaper_path = configure_wallpaper_path(title)
+	download_wallpaper(str(url), wallpaper_path)
+	change_local_wallpaper(wallpaper_path)
+
+
+if __name__ == "__main__":
+	main()
